80c73487dc4db30ab6419ea6f6e434222c56e4a8
WindCrazy <458709423@qq.com>
2023-08-05 20:15:29 +0800

    set up UMultiplayerSessionsSubsystem

--- Plugins/MultiplayerSessions/Source/MultiplayerSessions/Public/MultiplayerSessionsSubsystem.h
+++ Plugins/MultiplayerSessions/Source/MultiplayerSessions/Public/MultiplayerSessionsSubsystem.h
@@ -3,7 +3,10 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "OnlineSessionSettings.h"
+#include "Interfaces/OnlineSessionInterface.h"
 #include "Subsystems/GameInstanceSubsystem.h"
+
 #include "MultiplayerSessionsSubsystem.generated.h"
 
 /**
@@ -13,5 +16,46 @@ UCLASS()
 class MULTIPLAYERSESSIONS_API UMultiplayerSessionsSubsystem : public UGameInstanceSubsystem
 {
 	GENERATED_BODY()
-	
+public:
+	UMultiplayerSessionsSubsystem();
+
+	/**
+	 * @brief Create game session. The function will be called by Menu class
+	 * @param NumPublicConnections Specify the number of players that can join our game
+	 * @param MatchType Choose a particular match type
+	 */
+	void CreateSession(int32 NumPublicConnections, FString MatchType);
+	/**
+	 * @brief Find game session. The function will be called by Menu class
+	 * @param MaxSearch Specify the number of search results
+	 */
+	void FindSession(int32 MaxSearch);
+	void JoinSession(const FOnlineSessionSearchResult& SessionSearchResult);
+	void DestroySession();
+	void StartSession();
+protected:
+	// Callback Funcions
+	//Internal callbacks for the delegates we'll add to the Online Session Interface delegate list.
+	//These don't need to be called outside this class.
+	void OnCreateSessionComplete(FName SessionName, bool bWasSuccessful);
+	void OnFindSessionsComplete( bool bWasSuccessful);
+	void OnJoinSessionComplete(FName SessionName, EOnJoinSessionCompleteResult::Type Result);
+	void OnDestroySessionComplete(FName SessionName, bool bWasSuccessful);
+	void OnStartSessionComplete(FName SessionName, bool bWasSuccessful);
+
+private:
+	IOnlineSessionPtr SessionInterface;
+
+	//To add to the Online Session Interface delegate list.
+	//we 'll bind our MultiplayerSessionsSubsystem internal callbacks to these.
+	FOnCreateSessionCompleteDelegate CreateSessionCompleteDelegate;
+	FDelegateHandle CreateSessionCompleteDelegateHandle;
+	FOnFindSessionsCompleteDelegate FindSessionsCompleteDelegate;
+	FDelegateHandle FindSessionsCompleteDelegateHandle;
+	FOnJoinSessionCompleteDelegate JoinSessionCompleteDelegate;
+	FDelegateHandle JoinSessionCompleteDelegateHandle;
+	FOnDestroySessionCompleteDelegate DestroySessionCompleteDelegate;
+	FDelegateHandle DestroySessionCompleteDelegateHandle;
+	FOnStartSessionCompleteDelegate StartSessionCompleteDelegate;
+	FDelegateHandle StartSessionCompleteDelegateHandle;
 };
